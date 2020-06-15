7ca053781054be8dd907fff372b03b6c818b219e
Jake Witcher <jjwitcher09@gmail.com>
2020-06-15 01:08:45 -0400

    day 1, part 1

--- advent_of_code/src/main.rs
+++ advent_of_code/src/main.rs
@@ -1,3 +1,13 @@
+use day_1;
+
 fn main() {
-    println!("Hello, world!");
+    process_day_1();
+}
+
+fn process_day_1() {
+    if let Some(n) = day_1::run("./day_1/data/input.txt") {
+        println!("Frequency: {}", n);
+    } else {
+        println!("Frequency not found");
+    }
 }
--- day_1/src/lib.rs
+++ day_1/src/lib.rs
@@ -1,7 +1,73 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+pub fn run(filename: &str) -> Option<isize> {
+    if let Ok(lines) = read_lines(filename) {
+        return Some(calculate_frequency(lines))
+    }
+
+    None
+}
+
+fn calculate_frequency<T>(lines: T) -> isize
+where T: Iterator<Item = Result<String, io::Error>>
+{
+    let mut count = 0;
+
+    for line in lines {
+        if let Some(n) = line.ok().and_then(parse_frequency_change) {
+            count += n;
+        }
+    }
+
+    count
+}
+
+fn parse_frequency_change(line: String) -> Option<isize> {
+    line.parse::<isize>().ok()
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>
+{
+    match File::open(filename) {
+        Ok(file) => Ok(io::BufReader::new(file).lines()),
+        Err(err) => {
+            println!("{}", err);
+            Err(err)
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn succeeds_parsing_positive_frequency_change() {
+        let actual = parse_frequency_change("+5".to_string());
+        
+        assert_eq!(Some(5), actual);
+    }
+
+    #[test]
+    fn succeeds_parsing_negative_frequency_change() {
+        let actual = parse_frequency_change("-5".to_string());
+        
+        assert_eq!(Some(-5), actual);
+    }
+
+    #[test]
+    fn calculates_frequency_positive_numbers() {
+        let actual = 
+            calculate_frequency(
+                vec![
+                    Ok("+1".to_string()), 
+                    Ok("+3".to_string()), 
+                    Ok("+6".to_string())
+                ].into_iter());
+
+        assert_eq!(10, actual);
     }
 }
